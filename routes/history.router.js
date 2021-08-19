d68d2e41b7d5ec52608e3109d47e7097b5b1bfa5
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-08-20 01:00:27 +0530

    refactor

--- routes/history.router.js
+++ routes/history.router.js
@@ -14,12 +14,3 @@ router
   .post(authVerify, postHistoryController);
 
 module.exports = { router };
-
-// new
-("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjMGYwMTI1MjU0YzM2MjAwNDQwODYiLCJpYXQiOjE2MjkyMjg4MTYsImV4cCI6MTYyOTMxNTIxNn0.QoE6euVuPgBmcTHdA46a8ctFzUrlt-Bad-1Q0yAib2k");
-
-// old
-("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFiZmY4ZDU5MDUzMTJjNGM3NzI4NmIiLCJpYXQiOjE2MjkyMjY3ODYsImV4cCI6MTYyOTMxMzE4Nn0.Aq66AeDNRFm55RZZUapqGn_K4-S6ZBzr23Ylg3xXVXI");
-
-// Video-library
-("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFkMzA4OGQzNDdlYzI3MmM3OWRhYzUiLCJpYXQiOjE2MjkzMDMxODUsImV4cCI6MTYyOTM4OTU4NX0.rrkGqNuj-bXj2uiuHiNxurNdtJ3lX8CMpQp70E_f2NM");
